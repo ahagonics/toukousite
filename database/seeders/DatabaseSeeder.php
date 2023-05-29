db5a1d16ba9890abb3ebc2b93301df8d39c9ea78
momijilab <nihiru4869@gmail.com>
2023-05-29 17:32:17 +0900

    漫画テーブル、漫画モデル、漫画seeder、factoryの追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            GenresTableSeeder::class
+            GenresTableSeeder::class,
+            MangasSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
