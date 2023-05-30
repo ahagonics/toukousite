697c98414e1a5ad5cecfafd191e4e7a6d09c2c0d
momijilab <nihiru4869@gmail.com>
2023-05-30 10:30:34 +0900

    Novelsの追加

---
+++ database/factories/NovelsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Novels>
+ */
+class NovelsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->realText(10),
+            'genreid' => fake()->numberBetween(1,3),
+            'userid' => fake()->numberBetween(1,10),
+            'review' => fake()->numberBetween(1,5),
+            'created_at' => fake()->dateTime()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenresTableSeeder::class,
             MangasSeeder::class,
+            NovelsSeeder::class,
             ToukouUsersTableSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
